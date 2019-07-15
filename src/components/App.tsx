481a2b6199cd642457f05ffede26e5ad9022df83
Mek Karpeles <michael.karpeles@gmail.com>
2019-07-14 17:26:46 -0700

    adding fonts and main styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
+const reactLogo = require("./../assets/img/fox.png");
 import "./../assets/scss/App.scss";
 
 class App extends React.Component<{}, undefined> {
     public render() {
         return (
             <div className="app">
-                <h1>Hello World!</h1>
-                <p>Foo to the barz</p>
-                <img src={reactLogo} height="480"/>
+	      <h1 class="logo">
+                <img src={reactLogo} height="60"/>
+		Skillsera
+	      </h1>
+              <h2 class="tagline">
+	        A free community-powered encyclopedia which teaches educational<br/>concepts by connecting bite-sized youtube video explanations.
+	      </h2>
             </div>
         );
     }
